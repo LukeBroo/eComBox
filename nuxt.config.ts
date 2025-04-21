cacb0cb203aec5dab61578d22f2ece660f486df2
LukeBroo <lukaszbrodowski15@gmail.com>
2025-04-21 23:34:06 +0200

    feat: include @nuxt/fonts into config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,7 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['@/assets/styles/main.scss'],
-  vite: {
-    css: {
-      preprocessorOptions: {
-        scss: {
-          additionalData: `@use "@/assets/styles/variables" as *;`,
-        },
-      },
-    },
-  },
   modules: [
     '@nuxt/content',
     '@nuxt/eslint',
@@ -21,4 +11,19 @@ export default defineNuxtConfig({
     '@nuxt/test-utils',
     '@pinia/nuxt',
   ],
+  fonts: {
+    defaults: {
+      weights: [200, 400, 600, 800],
+      styles: ['normal', 'italic'],
+    },
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "@/assets/styles/variables" as *;`,
+        },
+      },
+    },
+  },
 })
