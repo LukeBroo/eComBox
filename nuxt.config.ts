aadf34484abe1de2de1bfcb855014adcf5c835a5
LukeBroo <lukaszbrodowski15@gmail.com>
2025-04-21 19:39:26 +0200

    chore: add scss reset & variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,16 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-
+  css: ['@/assets/styles/main.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "@/assets/styles/variables" as *;`,
+        },
+      },
+    },
+  },
   modules: [
     '@nuxt/content',
     '@nuxt/eslint',
