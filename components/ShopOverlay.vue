2fa350ef654188413841704d112bbe7204ce5804
LukeBroo <lukaszbrodowski15@gmail.com>
2025-04-22 17:55:08 +0200

    feat: split header into components

---
+++ components/ShopOverlay.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="overlay" />
+</template>
+
+<style scoped lang="scss">
+.overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.4);
+  backdrop-filter: blur(2px);
+  z-index: 5;
+}
+</style>
